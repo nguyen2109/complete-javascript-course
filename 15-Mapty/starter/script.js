3529af599b41a69f9b342e8c4e7a427a885351ea
“nguyen2109” <n21091999@gmail.com>
2023-07-21 23:02:07 +0700

    a

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude } = pos.coords;
+      const { longitude } = pos.coords;
+      //const coords = [latitude, longitude];
+
+      const coords = [20.9896224, 105.7852105];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Bạn chưa đồng ý vị trí!');
+    }
+  );
+}
